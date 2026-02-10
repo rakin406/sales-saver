495bf4bd7870b64987a535177f00edf24c6acfe3
Rakin Rahman <rakinhacker@gmail.com>
2026-02-10 12:08:24 +0600

    feat: use HibernateUtil

--- src/test/java/com/rakin/db/SaleTest.java
+++ src/test/java/com/rakin/db/SaleTest.java
@@ -1,35 +1,14 @@
 package com.rakin.db;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
 
 class SaleTest {
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
-    @BeforeEach
-    protected void init() {
-        // A SessionFactory is set up once for an application!
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
-                .build();
-        try {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-        } catch (Exception e) {
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-    }
-
-    @AfterEach
-    protected void teardown() {
-        if (sessionFactory != null) {
-            sessionFactory.close();
-        }
+    @AfterAll
+    protected static void teardown() {
+        HibernateUtil.shutdown();
     }
 
     @org.junit.jupiter.api.Test
