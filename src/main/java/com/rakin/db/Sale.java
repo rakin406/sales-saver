11fde69ec451bfe1ba5daa20f19714a4eb8ccffa
Rakin Rahman <rakinhacker@gmail.com>
2026-02-10 17:58:52 +0600

    feat: use sequence for id generation

--- src/main/java/com/rakin/db/Sale.java
+++ src/main/java/com/rakin/db/Sale.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Table(name = "sales")
 public class Sale {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false)
