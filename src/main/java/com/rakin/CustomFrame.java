370ed3ea6dee23e110bebc16740753cf4670c03c
Rakin Rahman <rakinhacker@gmail.com>
2026-02-11 17:55:20 +0600

    feat: add text field

--- src/main/java/com/rakin/CustomFrame.java
+++ src/main/java/com/rakin/CustomFrame.java
@@ -2,6 +2,7 @@ package com.rakin;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.NumberFormat;
 
 public class CustomFrame extends JFrame {
     CustomFrame() {
@@ -15,5 +16,23 @@ public class CustomFrame extends JFrame {
         // Set colors
         this.getContentPane().setBackground(new Color(245, 245, 245));
         this.getContentPane().setForeground(new Color(17, 17, 17));
+
+        JPanel panel = new JPanel();
+
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        JFormattedTextField amountTextField = new JFormattedTextField(currencyFormat);
+        amountTextField.setName("Amount");
+        amountTextField.setColumns(10);
+
+        JLabel amountLabel = new JLabel("Amount:");
+        amountLabel.setLabelFor(amountTextField);
+
+        panel.add(amountLabel);
+        panel.add(amountTextField);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(panel);
+
+        this.getContentPane().add(mainPanel, BorderLayout.CENTER);
     }
 }
